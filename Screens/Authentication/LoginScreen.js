4ce3bc1123d1d4c1cb47c80cf4f882ded9a45167
ZackTheManiac <zhu.yicheng@yahoo.com>
2023-05-17 17:33:01 +0800

    Additional style changes to login screen

--- Screens/Authentication/LoginScreen.js
+++ Screens/Authentication/LoginScreen.js
@@ -1,39 +1,124 @@
-import * as React from "react";
-import { Text, View, Image, TextInput, TouchableOpacity } from "react-native";
-import { useState } from "react";
+// import * as React from "react";
+// import { Text, View, Image, TextInput, TouchableOpacity } from "react-native";
+// import { useState } from "react";
 
-const LoginScreen = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+// const LoginScreen = () => {
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+
+//   return (
+//     <View>
+//       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+//         <Image
+//           source={require("../../assets/logo.png")}
+//           style={{ width: 500, height: 500 }}
+//         />
+//         <TextInput
+//           style={{ height: 40 }}
+//           placeholder="Email"
+//           onChangeText={(newText) => setEmail(newText)}
+//           defaultValue={email}
+//         />
+//         <TextInput
+//           style={{ height: 40 }}
+//           placeholder="Password"
+//           onChangeText={(newText) => setPassword(newText)}
+//           defaultValue={password}
+//         />
+//       </View>
+//       <TouchableOpacity>
+//         <Text>Login</Text>
+//       </TouchableOpacity>
+//       <TouchableOpacity>
+//         <Text>Register</Text>
+//       </TouchableOpacity>
+//     </View>
+//   );
+// };
+
+// export default LoginScreen;
+
+import React, { useContext, useState } from "react";
+import { View, Text, TouchableOpacity, Image, StyleSheet } from "react-native";
+import FormInput from "../../Components/Inputs/FormInput";
+import FormButton from "../../Components/Buttons/FormButton";
+
+const LoginScreen = ({ navigation }) => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
 
   return (
-    <View>
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Image
-          source={require("../../assets/logo.png")}
-          style={{ width: 500, height: 500 }}
-        />
-        <TextInput
-          style={{ height: 40 }}
-          placeholder="Email"
-          onChangeText={(newText) => setEmail(newText)}
-          defaultValue={email}
-        />
-        <TextInput
-          style={{ height: 40 }}
-          placeholder="Password"
-          onChangeText={(newText) => setPassword(newText)}
-          defaultValue={password}
-        />
-      </View>
-      <TouchableOpacity>
-        <Text>Login</Text>
+    <View style={styles.container}>
+      <Image source={require("../../assets/logo.png")} style={styles.logo} />
+      <Text style={styles.text}>KingPin</Text>
+
+      <FormInput
+        labelValue={email}
+        onChangeText={(userEmail) => setEmail(userEmail)}
+        placeholderText="Email"
+        iconType="user"
+        keyboardType="email-address"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+
+      <FormInput
+        labelValue={password}
+        onChangeText={(userPassword) => setPassword(userPassword)}
+        placeholderText="Password"
+        iconType="lock"
+        secureTextEntry={true}
+      />
+
+      <FormButton buttonTitle="Sign In" onPress={() => {}} />
+
+      <TouchableOpacity style={styles.forgotButton} onPress={() => {}}>
+        <Text style={styles.navButtonText}>Forgot Password?</Text>
       </TouchableOpacity>
-      <TouchableOpacity>
-        <Text>Register</Text>
+
+      <TouchableOpacity
+        style={styles.forgotButton}
+        onPress={() => navigation.navigate("Signup")}
+      >
+        <Text style={styles.navButtonText}>
+          Don't have an acount? Create here
+        </Text>
       </TouchableOpacity>
     </View>
   );
 };
 
 export default LoginScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#f9fafd",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+  logo: {
+    height: 150,
+    width: 150,
+    resizeMode: "cover",
+  },
+  text: {
+    fontFamily: "Kufam-SemiBoldItalic",
+    fontSize: 28,
+    marginBottom: 10,
+    color: "#051d5f",
+  },
+  navButton: {
+    marginTop: 15,
+  },
+  forgotButton: {
+    marginVertical: 35,
+  },
+  navButtonText: {
+    fontSize: 18,
+    fontWeight: "500",
+    color: "#2e64e5",
+    fontFamily: "Lato-Regular",
+  },
+});
