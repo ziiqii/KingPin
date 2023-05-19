14d036e29e2b6195c002f45c28fba5150853a85f
Ziqi <ziiiqiii@gmail.com>
2023-05-19 14:49:37 +0800

    Add temporary square icon. Rearrange bottom tabs.

--- Screens/Home/HomeScreen.js
+++ Screens/Home/HomeScreen.js
@@ -8,8 +8,8 @@ const Tab = createBottomTabNavigator();
 const HomeScreen = () => {
   return (
     <Tab.Navigator>
+      <Tab.Screen name="Bowl" component={WelcomeScreen} />
       <Tab.Screen name="Balls" component={BallsScreen} />
-      <Tab.Screen name="Welcome" component={WelcomeScreen} />
       <Tab.Screen name="Settings" component={SettingsScreen} />
     </Tab.Navigator>
   );
