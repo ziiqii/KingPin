94fed7ed5ae032c01d25d7f98ba2ed92346f5738
Ziqi <ziiiqiii@gmail.com>
2023-05-19 14:25:42 +0800

    Add screen for balls

--- Screens/Home/HomeScreen.js
+++ Screens/Home/HomeScreen.js
@@ -3,12 +3,14 @@ import { auth } from "../../firebase";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import WelcomeScreen from "../Welcome/WelcomeScreen";
 import SettingsScreen from "../Settings/SettingsScreen";
+import BallsScreen from "../BallsScreen/BallsScreen";
 
 const Tab = createBottomTabNavigator();
 
 const HomeScreen = ({ navigation }) => {
   return (
     <Tab.Navigator>
+      <Tab.Screen name="Balls" component={BallsScreen} />
       <Tab.Screen name="Welcome" component={WelcomeScreen} />
       <Tab.Screen name="Settings" component={SettingsScreen} />
     </Tab.Navigator>
