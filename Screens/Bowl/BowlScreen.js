3ec52bae9ab4c64902846ba5e24d9a77d8cd353e
ZackTheManiac <zhu.yicheng@yahoo.com>
2023-05-23 11:10:17 +0800

    Combined Stack and Tab navigation for Bowl screen navigation

--- Screens/Bowl/BowlScreen.js
+++ Screens/Bowl/BowlScreen.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 
-const BowlScreen = () => {
+const BowlScreen = ({ navigation }) => {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate("StartBowlScreen")}>
         <Text>Start Game</Text>
       </TouchableOpacity>
       <TouchableOpacity>
