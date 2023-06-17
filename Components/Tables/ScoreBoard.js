438f332de0505063409f2b8ecdd802fdd3f02572
Ziqi <ziiiqiii@gmail.com>
2023-06-17 19:08:01 +0800

    Scoreboard displays realtime scores

--- Components/Tables/ScoreBoard.js
+++ Components/Tables/ScoreBoard.js
@@ -1,28 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { Text, View } from "react-native";
-import {
-  Table,
-  TableWrapper,
-  Row,
-  Rows,
-  Col,
-  Cols,
-  Cell,
-} from "react-native-reanimated-table";
+import { Table, Row } from "react-native-reanimated-table";
 import styles from "./ScoreBoard.style";
 
 import { db } from "../../firebase";
 import { getAuth } from "firebase/auth";
-import {
-  doc,
-  collection,
-  onSnapshot,
-  query,
-  orderBy,
-  where,
-} from "firebase/firestore";
+import { doc, onSnapshot, query } from "firebase/firestore";
+
+import parseGame from "../../Functions/parseGame";
 
 const ScoreBoard = (props) => {
+  const framesTop = [1, 2, 3, 4, 5, 6];
+  const framesBot = [7, 8, 9, 10, "TOT"];
+  const [rollsTop, setRollsTop] = useState([]);
+  const [scoresTop, setScoresTop] = useState([]);
+  const [rollsBot, setRollsBot] = useState([]);
+  const [scoresBot, setScoresBot] = useState([]);
+
   useEffect(() => {
     const auth = getAuth();
     const userDoc = doc(db, "users", auth.currentUser?.email);
@@ -30,23 +24,16 @@ const ScoreBoard = (props) => {
     const gameQuery = query(gameDoc);
 
     const unsubscribeGameListener = onSnapshot(gameQuery, (doc) => {
-      console.log("Current data: ", doc.data());
-      // Function to parse a game (10 frames) into 6 arrays to be displayed below
-      // parsedGame is an array of the 6 arrays
-      // parsedGame = function(doc.data())
-      // -> call parsedGame[0], parsedGame[1] etc
+      // parsedGame is an array of the 4 arrays to be displayed
+      let parsedGame = parseGame(doc.data());
+      setRollsTop(parsedGame[0]);
+      setScoresTop(parsedGame[1]);
+      setRollsBot(parsedGame[2]);
+      setScoresBot(parsedGame[3]);
     });
     // Cleanup listener
     return () => unsubscribeGameListener();
   }, []);
-
-  const framesTop = ["1", "2", "3", "4", "5", "6"];
-  const rollsTop = ["2", "5", "8", "1", "5", "/", "X", "", "8", "/", "2", "5"];
-  const scoresTop = ["7", "16", "36", "56", "68", "75"];
-
-  const framesBot = ["7", "8", "9", "10", "TOT"];
-  const rollsBot = ["7", "/", "6", "3", "8", "/", "7", "/", "4", "?"];
-  const scoresBot = ["91", "100", "117", "131", "131"];
 
   return (
     <View style={styles.container}>
