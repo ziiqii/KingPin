b6cb4a4f8fb662de59ecff76ac3e422d22748f99
ZackTheManiac <zhu.yicheng@yahoo.com>
2023-07-06 12:09:19 +0800

    Add navigation to ballsScreen and ArsenalScreen

---
+++ Screens/Balls/ArsenalScreen.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const ArsenalScreen = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#36393f",
+      }}
+    >
+      <Text style={{ color: "white" }}>Arsenal</Text>
+    </View>
+  );
+};
+
+export default ArsenalScreen;
--- navigators/HomeTab.js
+++ navigators/HomeTab.js
@@ -1,9 +1,9 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import BowlScreen from "../Screens/Bowl/BowlScreen";
 import SettingsScreen from "../Screens/Settings/SettingsScreen";
-import BallsScreen from "../Screens/Balls/BallsScreen";
 import GuideScreen from "../Screens/Guide/GuideScreen";
 import TrackerScreen from "../Screens/Tracker/TrackerScreen";
+import BallsMainScreen from "../Screens/Balls/BallsMainScreen";
 
 const Tab = createBottomTabNavigator();
 
@@ -14,7 +14,7 @@ export default HomeTab = () => {
       lazy="true"
       screenOptions={{ headerShown: false }}
     >
-      <Tab.Screen name="Balls" component={BallsScreen} />
+      <Tab.Screen name="Balls" component={BallsMainScreen} />
       <Tab.Screen name="Spare Guide" component={GuideScreen} />
       <Tab.Screen name="Bowl" component={BowlScreen} />
       <Tab.Screen name="Analytics" component={TrackerScreen} />
